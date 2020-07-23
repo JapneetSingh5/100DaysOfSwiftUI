fa67e66684902a6e93e83855296fc80fc8e61e55
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-24 02:35:37 +0530

    ✅ Gesture Animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,20 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
+    @State private var dragAmount2 = CGSize.zero
+    let letters = Array("Hello World!")
     
     var body: some View {
-         Button("Tap Me") {
-            withAnimation(Animation.interpolatingSpring(stiffness: 5, damping: 10)
-                .repeatCount(3, autoreverses: true)) {
-                       self.animationAmount += 360
-                   }
-               }
-               .padding(50)
-               .background(Color.red)
-               .foregroundColor(.white)
-               .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
+        VStack {
+            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { self.dragAmount = $0.translation }
+                        .onEnded {_ in
+                            withAnimation(Animation.spring(response: 1, dampingFraction: 0.5, blendDuration: 0))
+                                {
+                                  self.dragAmount = .zero
+                                }
+                        }
+            )
+            
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(self.letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(self.enabled ? Color.blue : Color.red)
+                        .offset(self.dragAmount2)
+                        .animation(Animation.default.delay(Double(num) / 20))
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount2 = $0.translation }
+                    .onEnded { _ in
+                        self.dragAmount2 = .zero
+                        self.enabled.toggle()
+                    }
+            )
+        }
     }
 }
 
