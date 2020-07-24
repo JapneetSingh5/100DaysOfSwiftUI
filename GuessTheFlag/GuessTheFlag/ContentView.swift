4d77c0b96fbe4f3713c580ff6f153a6a28657a71
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-25 02:08:29 +0530

    🌟 Added Animations

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -32,6 +32,11 @@ struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var flagRotationRight: Double = 0
+    @State private var flagRotationWrong: Double = 0
+    @State private var jumpOffset: CGFloat = 0
+    @State private var opacity: Double = 0.9
+    @State private var submit: Bool = false
     
     var body: some View {
         ZStack{
@@ -43,22 +48,28 @@ struct ContentView: View {
                 }
                 ForEach( 0..<3){ number in
                     Button(action: {
+                        self.submit = true
                         self.flagTapped(number)
                     }){
                         Image(self.countries[number])
                                 .renderingMode(.original)
-                                .flagImage()
+                            .flagImage().opacity(number != self.correctAnswer ? self.opacity : 0.9)
+                    }.rotation3DEffect( Angle.degrees(number==self.correctAnswer ? self.flagRotationRight : self.flagRotationWrong), axis: number==self.correctAnswer ? (x:0, y:1,z:0) : (x:1, y:0,z:0))
+                        .offset(y: number == self.correctAnswer ? self.jumpOffset : 0)
                         
-                    }
                 }
                 Text("Score: \(correctCount) / \(questionCount)").fontWeight(.black).font(.headline).foregroundColor(.white)
+                if questionCount>0 {
+                    Text("\(scoreTitle)").font(.headline).foregroundColor(.white)
+                    if(self.submit){
+                        Button("Continue"){
+                        self.askQuestion()
+                            self.submit = false
+                                }
+                    }
+                }
                 Spacer()
             }
-        }
-        .alert(isPresented: $showingScore){
-            Alert(title: Text(scoreTitle), message: Text("Your score is \(correctCount)/\(questionCount)"), dismissButton: .default(Text("Continue")){
-                self.askQuestion()
-            })
         }
     }
     
@@ -67,13 +78,32 @@ struct ContentView: View {
         if number==correctAnswer{
             scoreTitle = "Correct! Great job!"
             correctCount += 1
+            withAnimation(
+                Animation.easeInOut(duration: 1)
+            ){
+                 self.flagRotationRight += 360
+                 self.opacity = 0.25
+            }
+            self.submit = true
         }else{
-            scoreTitle = "Wrong :( That is the flag of \(countries[number])"
+            scoreTitle = "Wrong :( That was the flag of \(countries[number])"
+            self.opacity = 0.25
+            withAnimation(
+                Animation.easeInOut(duration: 1).repeatCount(1, autoreverses: true)
+            ){
+                 self.flagRotationWrong += 360
+                 self.jumpOffset += -10
+            }
+            self.submit = true
         }
         showingScore = true
     }
     
     func askQuestion(){
+        self.jumpOffset = 0
+        self.flagRotationRight = 0
+        self.flagRotationWrong = 0
+        self.opacity = 1.0
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
