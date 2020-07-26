2b768359ca841737610ad3c5d0ed6137c5b909d5
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-26 12:49:03 +0530

    Codable and UserDefaults

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,54 +8,30 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct User: Codable {
+    var firstName: String
+    var lastName: String
 }
 
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-    
-    var body: some View {
-        VStack {
-            Text("Second View")
-            Button("Dismiss") {
-                self.presentationMode.wrappedValue.dismiss()
-            }
-        }
-    }
-}
-
-struct NestedView: View{
-    @ObservedObject var user2: User
-    
-    var body: some View{
-        VStack {
-            Text("Your name is \(user2.firstName) \(user2.lastName).")
-
-            TextField("First name", text: $user2.firstName)
-            TextField("Last name", text: $user2.lastName)
-        }
-    }
-
-}
 
 struct ContentView: View {
-    @ObservedObject var user = User()
-    @State private var showingSheet = false
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
 
     var body: some View {
-        VStack {
-            NestedView(user2: user)
-            Button("Show Sheet") {
-                self.showingSheet.toggle()
-            }
-            .sheet(isPresented: $showingSheet) {
-                SecondView()
+        Button("Save User") {
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
             }
         }
     }
 }
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
