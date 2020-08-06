cdd0df71b710b3654579db7d1b1bf00f115693a2
Japneet Singh <singhjapneetssa@gmail.com>
2020-08-06 17:01:44 +0530

    disabled Form elements/ sections

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -37,41 +37,25 @@ struct Result: Codable {
 }
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=diljit+dosanjh&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
+    @State private var username = ""
+    @State private var email = ""
 
-                    // everything is good, so we can exit
-                    return
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
                 }
+                .disabled(username.isEmpty || email.isEmpty)
             }
             
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
 
-    }
-    
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }.onAppear(perform: loadData)
+        }
     }
 }
 
