29cd5ea416c035593f0c01f6dd50b7313fee61c5
Japneet Singh <singhjapneetssa@gmail.com>
2020-08-15 00:50:39 +0530

    ⚙️CoreDataProject init'd

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Japneet Singh on /158/20.
+//  Copyright © 2020 Japneet Singh. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
