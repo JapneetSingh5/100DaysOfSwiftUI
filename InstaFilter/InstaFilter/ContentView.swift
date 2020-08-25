521dd4a485a4bea91fb5351036f5565aa4ae2086
Japneet Singh <singhjapneetssa@gmail.com>
2020-08-25 22:10:39 +0530

    ⭐️ playing with CIImageFilters and Binding

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -7,49 +7,55 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var blurAmount: CGFloat = 0
     @State private var showingActionSheet = false
     @State private var backgroundColor = Color.white
+    @State private var image:  Image?
+    @State private var twirlAmount: Double = 0
 
     var body: some View {
-        let blur = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-        },
-            set: {
-                self.blurAmount = $0
-                print("Blur amount is now \($0)")
-        })
+        var twirl = Binding<Double>(
+            get:{
+                self.twirlAmount
+            },
+            set:{
+                self.twirlAmount = $0
+                self.loadImage()
+            }
+        )
         return VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-                .frame(width: 300, height: 300)
-                .background(self.backgroundColor)
-                .onTapGesture {
-                    self.showingActionSheet = true
-            }
-            .actionSheet(isPresented: $showingActionSheet){
-                ActionSheet(title: Text("Change Background"), message: Text("Select a color"), buttons: [
-                    .default(Text("Red"), action: {
-                        self.backgroundColor = Color.red
-                    }),
-                    .default(Text("Blue"), action: {
-                        self.backgroundColor = Color.blue
-                    }),
-                    .default(Text("Green"), action: {
-                        self.backgroundColor = Color.green
-                    }),
-                    .cancel(),
-                    .destructive(Text("Cancel")){
-                        print("Destructive Cancel")
-                    }
-                ])
-            }
+            image?
+            .resizable()
+            .scaledToFit()
+            .drawingGroup()
+            Slider(value: twirl, in: 0...1000)
+            .padding()
+        }
             
-
-            Slider(value: blur, in: 0...20)
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage(){
+        
+        guard let inputImage = UIImage(named: "Example") else {return}
+        
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(self.twirlAmount, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width/2 , y: inputImage.size.height/2), forKey: kCIInputCenterKey)
+        guard let outputImage = currentFilter.outputImage else {return}
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
         }
     }
 }
