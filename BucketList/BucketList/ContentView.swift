6d01dfb8f5b1e0e2ebae710fcf7ef8486bef05d0
Japneet Singh <singhjapneetssa@gmail.com>
2020-09-09 00:21:05 +0530

    🌟 Local Authentication

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -81,34 +81,46 @@ User(firstName: "Jasper", lastName: "Singh")
     
     var body: some View {
         ZStack{
-            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-            .edgesIgnoringSafeArea(.all)
-            Circle()
-            .fill(Color.blue)
-            .opacity(0.3)
-            .frame(width: 32, height: 32)
-            VStack{
-                Spacer()
-                HStack{
+            if isUnlocked{
+                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+                Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+                VStack{
                     Spacer()
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.coordinate = self.centerCoordinate
-                        newLocation.title = "Example Location"
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }){
-                        Image(systemName: "plus")
+                    HStack{
+                        Spacer()
+                        Button(action: {
+                            let newLocation = CodableMKPointAnnotation()
+                            newLocation.coordinate = self.centerCoordinate
+                            newLocation.title = "Example Location"
+                            self.locations.append(newLocation)
+                            self.selectedPlace = newLocation
+                            self.showingEditScreen = true
+                        }){
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
             }
+            else{
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+
         }
         .onAppear(perform: loadData)
         .alert(isPresented: $showingPlaceDetails) {
