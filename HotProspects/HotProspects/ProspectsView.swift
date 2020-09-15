232ee51f041ebcde07470804c372685af12b09b3
Japneet Singh <singhjapneetssa@gmail.com>
2020-09-16 02:32:29 +0530

    🌟 Access control and processing QR Codes

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -7,9 +7,11 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     
     enum FilterType {
         case none, contacted, uncontacted
@@ -35,6 +37,25 @@ struct ProspectsView: View {
             return prospects.people.filter { !$0.isContacted }
         }
     }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+       self.isShowingScanner = false
+       switch result {
+       case .success(let code):
+           let details = code.components(separatedBy: "\n")
+           guard details.count == 2 else { return }
+
+           let person = Prospect()
+           person.name = details[0]
+           person.emailAddress = details[1]
+
+           self.prospects.people.append(person)
+       case .failure(let error):
+           print("Scanning failed")
+           print(error)
+       }
+    }
+    
     var body: some View {
         NavigationView {
             List {
@@ -45,18 +66,23 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+                            self.prospects.toggle(prospect)
+                        }
+                    }
                 }
             }
                 .navigationBarTitle(title)
                 .navigationBarItems(trailing: Button(action: {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    self.prospects.people.append(prospect)
+                    self.isShowingScanner = true
                 }) {
                     Image(systemName: "qrcode.viewfinder")
                     Text("Scan")
                 })
+        }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
         }
     }
 }
