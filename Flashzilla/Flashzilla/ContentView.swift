23a0d2104e64a0fba52194c0b38ed84d9d8f9ed5
Japneet Singh <singhjapneetssa@gmail.com>
2020-09-18 00:32:43 +0530

    ⭐️ Specific Accessibility Needs

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,20 +10,19 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
-
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     var body: some View {
-        Text("Hello, World!")
-            .onReceive(timer) { time in
-                if self.counter == 5 {
-                    self.timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-
-                self.counter += 1
+        HStack {
+            if differentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
             }
+
+            Text("Success")
+        }
+        .padding()
+        .background(differentiateWithoutColor ? Color.black : Color.green)
+        .foregroundColor(Color.white)
+        .clipShape(Capsule())
     }
 }
 
