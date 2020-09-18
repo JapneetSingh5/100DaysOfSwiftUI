712b1d247adcf1da52dae7934fa2203bf95d2ba1
Japneet Singh <singhjapneetssa@gmail.com>
2020-09-18 23:11:01 +0530

    ⭐️ Flashzilla MVP complete

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,20 +9,116 @@
 import SwiftUI
 import CoreHaptics
 
-struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    var body: some View {
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
-            }
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self
+                .offset(CGSize(width: 0, height: offset * 10))
+    }
+}
 
-            Text("Success")
+struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var timeRemaining = 100
+    @State private var isActive = true
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
         }
-        .padding()
-        .background(differentiateWithoutColor ? Color.black : Color.green)
-        .foregroundColor(Color.white)
-        .clipShape(Capsule())
+
+    }
+    
+    func resetCards() {
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
+    }
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Time: \(timeRemaining)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                .background(
+                    Capsule()
+                        .fill(Color.black)
+                        .opacity(0.75)
+                )
+                
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index]){
+                            withAnimation {
+                                self.removeCard(at: index)
+                                  }
+                        }
+                        .stacked(at: index, in: self.cards.count)
+                    }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
+                
+
+            }
+            
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                            .padding(.trailing, 250)
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                            .padding(.leading, 250)
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+            
+            
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            if self.cards.isEmpty == false {
+                self.isActive = true
+            }
+        }
     }
 }
 
