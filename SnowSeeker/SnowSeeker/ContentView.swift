f73c937980a02e50ea9061b922718a34d0088dce
Japneet Singh <singhjapneetssa@gmail.com>
2020-09-22 21:06:28 +0530

    ✅ Added Filter (challenges complete)

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -22,46 +22,145 @@ struct ContentView: View {
     @ObservedObject var favorites = Favorites()
     @State private var sortKey: Resort.SortBy = .none
     @State private var showSortMenu = false
+    @State private var showFilterMenu = false
+    @State private var showCountrySheet = false
+    @State private var filterPrice = 1
+    @State private var filterSize = 1
+    @State private var isFiltered = false
+    @State private var filterCountry = ["Austria": true, "Canada": true, "France": true, "Italy": true, "United States":true]
     
     var resorts: [Resort]{
-        Resort.sort(key: sortKey, resorts: Bundle.main.decode("resorts.json"))
+        self.filter(resorts: (Resort.sort(key: sortKey, resorts: Bundle.main.decode("resorts.json"))))
+    }
+    
+    let prices = [1,2,3]
+    let sizes = [1,2,3]
+    let countries = ["Austria", "Canada", "France", "Italy", "United States"]
+    
+    func sizeTransform(size: Int)->String {
+        switch size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    
+    func filter(resorts: [Resort]) -> [Resort]{
+        if self.isFiltered{
+            return resorts.filter({
+                (($0.price == self.filterPrice) && ($0.size == self.filterSize)) && (self.filterCountry[$0.country] ?? true)
+            })
+        }
+        else{
+            return resorts
+        }
     }
 
     var body: some View {
         NavigationView{
-            List(resorts){resort in
+            
+            List{
+                if self.showFilterMenu{
+                     withAnimation{
+                        VStack(alignment: .leading){
+                            
+                            Text("FILTER")
+                                .fontWeight(.bold)
+                            
+                            Button(action: {
+                                self.showCountrySheet = true
+                                self.isFiltered = true
+                            }){
+                                Text("FILTER BY COUNTRY")
+                                    .font(.headline)
+                                    .fontWeight(.semibold)
+                            }
+                            .padding(.vertical)
+                            .foregroundColor(.blue)
+                            
+                            Text("PRICE RANGE")
+                                .font(.caption)
+                                .fontWeight(.semibold)
+                            
+                            Picker(selection: self.$filterPrice, label: Text("Price Category"), content: {
+                                ForEach(prices, id:\.self){price in
+                                    Text(String.init(repeating: "$", count: price))
+                                }
+                            })
+                            .pickerStyle(SegmentedPickerStyle())
+                            
+                            Text("SIZE RANGE")
+                                .font(.caption)
+                                .fontWeight(.semibold)
+                            
+                            Picker(selection: self.$filterSize, label: Text("Size Category"), content: {
+                                ForEach(sizes, id:\.self){size in
+                                    Text(self.sizeTransform(size: size))
+                                }
+                            })
+                            .pickerStyle(SegmentedPickerStyle())
+                            
+                         }
+                        .padding(.vertical)
+                     }
+                 }
                 
-                NavigationLink(destination: ResortView(resort: resort)){
-                    Image(resort.country)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 25)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                    .overlay(RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.black, lineWidth: 1))
-                    
-                    VStack(alignment: .leading){
-                        Text(resort.name)
-                            .font(.headline)
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
+                if resorts.isEmpty{
+                    Text("Sorry! No matching results found.")
+                }
+                
+                ForEach(resorts){resort in
+                    NavigationLink(destination: ResortView(resort: resort)){
+                        Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.black, lineWidth: 1))
                         
-                    }
-                    .layoutPriority(1)
+                        VStack(alignment: .leading){
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                            
+                        }
+                        .layoutPriority(1)
 
-                    
-                    if self.favorites.contains(resort) {
-                        Spacer()
-                        Image(systemName: "heart.fill")
-                        .accessibility(label: Text("This is a favorite resort"))
-                            .foregroundColor(.red)
+                        
+                        if self.favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                            .accessibility(label: Text("This is a favorite resort"))
+                                .foregroundColor(.red)
+                        }
                     }
+                }
+                
+                if self.isFiltered{
+                    Button("RESET FILTER"){
+                        self.isFiltered = false
+                        self.showFilterMenu = false
+                    }
+                    .font(.headline)
+                    .foregroundColor(.red)
+                    .padding(.vertical)
                 }
                 
 
             }
             .navigationBarTitle("Resorts")
-            .navigationBarItems(trailing: Button(action: {
+            .navigationBarItems(leading: Button(action: {
+                self.showFilterMenu.toggle()
+                self.isFiltered = true
+            }){
+                Image(systemName: self.showFilterMenu ? "checkmark.circle.fill" :  "line.horizontal.3.decrease.circle" )
+                },
+                trailing: Button(action: {
                     self.showSortMenu = true
                 }){
                     Image(systemName: "arrow.up.arrow.down")
@@ -81,6 +180,9 @@ struct ContentView: View {
                         .cancel()
                     ])
             })
+                .sheet(isPresented: self.$showCountrySheet){
+                    MultiPickerView(choices: self.countries, selection: self.$filterCountry)
+            }
             
             WelcomeView()
             
---
+++ SnowSeeker/SnowSeeker/MultiPickerView.swift
@@ -0,0 +1,46 @@
+//
+//  MultiPickerView.swift
+//  SnowSeeker
+//
+//  Created by Japneet Singh on /229/20.
+//  Copyright © 2020 Japneet Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct MultiPickerView: View {
+    let choices: [String]
+    @Binding var selection: [String: Bool]
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(0..<choices.count){choiceNumber in
+                    HStack{
+                        Image(self.choices[choiceNumber])
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        Text(self.choices[choiceNumber])
+                        if(self.selection[self.choices[choiceNumber]] ?? true){
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+
+                    }
+                    .onTapGesture {
+                            self.selection[self.choices[choiceNumber]]?.toggle()
+                    }
+                }
+            }
+            .navigationBarItems(trailing: Button("Done"){
+                self.presentationMode.wrappedValue.dismiss()
+            })
+            .navigationBarTitle("Filter Countries")
+        }
+
+    }
+}
+
